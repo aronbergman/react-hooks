7f0b1948a74ec11d8dccc48dd799dad0a776d20f
Aron Bergman <brgmn@icloud.com>
2020-04-19 01:12:03 +0300

    Sending Http requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,21 +8,31 @@ const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([])
 
     const addIngredientHandler = ingredient => {
-        setUserIngredients(prevIngredient => [
-            ...prevIngredient,
-            {id: Math.random().toString(), ...ingredient}
-        ])
+        fetch('https://burger-builder-56e5a.firebaseio.com/ingredients2.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient), // [] в JSON
+            headers: {'Content-Type': 'application/json'}
+        }).then(res => {
+            return res.json() // JSON в норм js
+        }).then(res => {
+            setUserIngredients(prevIngredient => [
+                ...prevIngredient,
+                {id: res.name, ...ingredient}
+            ])
+        })
     }
 
     return (
         <div className="App">
             <IngredientForm
-                onRemoveItem={() => {}}
                 onAddIngredient={addIngredientHandler}/>
 
             <section>
                 <Search/>
-                <IngredientList ingredients={userIngredients}/>
+                <IngredientList
+                    onRemoveItem={() => {
+                    }}
+                    ingredients={userIngredients}/>
                 {/* Need to add list here! */}
             </section>
         </div>
